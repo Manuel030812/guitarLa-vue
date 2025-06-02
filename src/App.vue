588bcef4697226750733b391b769b4d02d06daa2
Manuel Lopez <aqnuelmagamer8@gmail.com>
2025-06-02 15:07:15 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+  const hola = 'Hola Mundo!';
+
+</script>
+
+<template>
+  <div class="text-center">
+    <h1 class="text-centar text-danger fs-1">{{ hola }}</h1>
+    <p>Welcome to your Vue.js app!</p>
+  </div>
+
+</template>
+
+<style scoped>
+
+
+</style>
